66b857ebf33ec62b4603fb8b22a50d7c9fe3f2ca
Colin Finck <colin@reactos.org>
2017-09-16 15:32:26 +0000

    [GIT-TOOLS] Add a GitHub Web Hook that updates our Git mirror and calls its "post-receive" hook just like Git would do if the commit was pushed to the mirror repository.
    This enables us to set up arbitrary post-receive hooks and keeps our Git setup independent from GitHub.
    If we ever moved away from it to a self-hosted Git, all hooks would continue to work without any reconfiguration.
    
    svn path=/trunk/git-tools/; revision=2354

---
+++ post-receive-webhook.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * PROJECT:     ReactOS GitHub Web Hook
+ * LICENSE:     GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
+ * PURPOSE:     Updates the mirror repository and calls the post-receive hook just like Git would do if the commit was pushed to the mirror repository.
+ * COPYRIGHT:   Copyright 2017 Colin Finck (colin@reactos.org)
+ */
+
+	// This must be run exclusively with HTTPS security enabled!
+	if (!array_key_exists("HTTPS", $_SERVER) || $_SERVER["HTTPS"] != "on")
+		die("TLS required");
+
+	// Verify that all necessary environment variables are set.
+	if (!array_key_exists("GIT_MIRROR_DIR", $_ENV) || !array_key_exists("GITHUB_SECRET", $_ENV))
+		die("Environment variables not set");
+
+	// Verify the signature format.
+	if (!array_key_exists("HTTP_X_HUB_SIGNATURE", $_SERVER) || substr($_SERVER["HTTP_X_HUB_SIGNATURE"], 0, 5) != "sha1=")
+		die("Wrong signature format");
+
+	// Verify the signature itself.
+	$http_signature = substr($_SERVER["HTTP_X_HUB_SIGNATURE"], 5);
+
+	$post_data = file_get_contents("php://input");
+	$valid_signature = hash_hmac("sha1", $post_data, $_ENV["GITHUB_SECRET"]);
+
+	if (!hash_equals($valid_signature, $http_signature))
+		die("Invalid signature");
+
+	// Verify the event.
+	if (!array_key_exists("HTTP_X_GITHUB_EVENT", $_SERVER)
+		die("No event");
+
+	// Respond to a ping event without doing anything else.
+	if ($_SERVER["HTTP_X_GITHUB_EVENT"] == "ping")
+		die("pong");
+
+	// Only handle "push" events from here onwards.
+	if ($_SERVER["HTTP_X_GITHUB_EVENT"] != "push")
+		die("Wrong event");
+
+	// Parse the JSON payload.
+	$payload = json_decode($_POST["payload"]);
+	if ($payload === NULL)
+		die("Invalid payload");
+
+	// Update the mirror repository.
+	chdir($_ENV["GIT_MIRROR_DIR"]);
+	$pp = popen("git pull &> /dev/null", "w");
+	$exit_code = pclose($pp);
+	if ($exit_code != 0)
+		die("git pull exited with code {$exit_code}");
+
+	// Call the post-receive hook.
+	$pp = popen("hooks/post-receive &> /dev/null", "w");
+	fwrite($pp, $payload->before . " " . $payload->after . " " . $payload->ref . "\n");
+	$exit_code = pclose($pp);
+	if ($exit_code != 0)
+		die("post-receive hook exited with code {$exit_code}");
+
+	// All done!
+	die("OK");
