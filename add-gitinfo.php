d4d7b6a60d284cddb00213d4682105bba0f0f205
Colin Finck <colin@reactos.org>
2018-04-04 08:12:38 +0200

    Let add-gitinfo.php consult Git itself instead of STDIN to find out about all revisions to add.
    This makes it simpler and more robust.
    A single missed revision can no longer bring the entire process to a standstill.

--- add-gitinfo.php
+++ add-gitinfo.php
@@ -4,7 +4,7 @@
  * PROJECT:     ReactOS Website
  * LICENSE:     GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
  * PURPOSE:     Populates the GitInfo database with information about commits to the master branch
- * COPYRIGHT:   Copyright 2017 Colin Finck (colin@reactos.org)
+ * COPYRIGHT:   Copyright 2017-2018 Colin Finck (colin@reactos.org)
  */
 
 	// Configuration
@@ -27,33 +27,31 @@
 		$dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_GITINFO, DB_USER, DB_PASS);
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		// STEP 1: Insert all revisions from stdin into the "master_revisions_todo" table.
+		// STEP 1: Get the latest master revision.
+		$newrev = trim(`git rev-parse refs/heads/master`);
+
+		// STEP 2: Get the latest revision added to the master_revisions table.
+		$oldrev_stmt = $dbh->query("SELECT rev_hash FROM master_revisions ORDER BY id DESC LIMIT 1");
+		$oldrev = $oldrev_stmt->fetchColumn();
+		if (!$oldrev)
+			throw new RuntimeException("master_revisions table is empty!");
+
+		// STEP 3: Get all revisions in between and add them to the "master_revisions_todo" table.
 		$todo_stmt = $dbh->prepare("REPLACE master_revisions_todo (oldrev, newrev) VALUES (:oldrev, :newrev)");
 		$todo_stmt->bindParam(":oldrev", $oldrev);
 		$todo_stmt->bindParam(":newrev", $newrev);
 
-		while (($line = fgets(STDIN)) !== FALSE)
+		$pp = popen("git rev-list --reverse $oldrev..$newrev", "r");
+		while (($line = fgets($pp)) !== FALSE)
 		{
-			$input = explode(" ", trim($line));
-			if ($input[2] != "refs/heads/master")
-				continue;
-
-			$oldrev = $input[0];
-			$newrev = $input[1];
-
-			// oldrev and newrev may have revisions in between, so we have to loop through the output of "git rev-list" to get them all.
-			$pp = popen("git rev-list --reverse $oldrev..$newrev", "r");
-			while (($line = fgets($pp)) !== FALSE)
-			{
-				$newrev = trim($line);
-				$todo_stmt->execute();
-				$oldrev = $newrev;
-			}
-
-			pclose($pp);
+			$newrev = trim($line);
+			$todo_stmt->execute();
+			$oldrev = $newrev;
 		}
 
-		// STEP 2: Process the "master_revisions_todo" table into a linear "master_revisions" table as far as we can.
+		pclose($pp);
+
+		// STEP 4: Process the "master_revisions_todo" table into a linear "master_revisions" table.
 		$newrev_stmt = $dbh->prepare("SELECT newrev FROM master_revisions_todo WHERE oldrev = (SELECT rev_hash FROM master_revisions ORDER BY id DESC LIMIT 1)");
 
 		$insert_stmt = $dbh->prepare("INSERT INTO master_revisions (rev_hash, author_name, author_email, commit_timestamp, message) VALUES (:rev_hash, :author_name, :author_email, FROM_UNIXTIME(:commit_timestamp), COMPRESS(:message))");
