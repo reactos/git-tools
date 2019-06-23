6b8e09de788641d04b6794a60396890e8012e2b5
Colin Finck <colin@reactos.org>
2019-06-23 13:55:04 +0200

    Update buildbot_sendchange.py for the new Buildmaster path and Python 3.
    Finally handles Unicode author names properly :)

--- buildbot_sendchange.py
+++ buildbot_sendchange.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # PROJECT:     buildbot_sendchange.py
 # LICENSE:     GPL-2.0 (https://spdx.org/licenses/GPL-2.0)
 # PURPOSE:     Inform BuildBot about Git commits and perform ReactOS-specific categorizing
 # COPYRIGHT:   Copyright 2007-2017 BuildBot Contributors
-#              Copyright 2017-2018 Colin Finck (colin@reactos.org)
+#              Copyright 2017-2019 Colin Finck (colin@reactos.org)
 #
 # Largely based on https://github.com/buildbot/buildbot-contrib/blob/9df6a1b6dae44eecbd56ed5d6d7ac6952d39066e/master/contrib/git_buildbot.py
 # but uses "buildbot sendchange" instead to do ReactOS-specific categorizing
@@ -28,13 +28,15 @@ for hookinput in sys.stdin:
     # Report all changes between oldrev and newrev
     revlist_pipe = subprocess.Popen("git rev-list --reverse %s..%s" % (oldrev, newrev), stdout=subprocess.PIPE, shell=True)
     for revhash in revlist_pipe.stdout:
-        revhash = revhash.rstrip()
+        revhash = revhash.decode('utf-8').rstrip()
 
         show_pipe = subprocess.Popen("git show --raw --pretty=full %s" % revhash, stdout=subprocess.PIPE, shell=True)
         files = []
         comments = []
 
         for line in show_pipe.stdout:
+            line = line.decode('utf-8')
+
             if line.startswith(4 * ' '):
                 comments.append(line[4:])
                 continue
@@ -59,5 +61,5 @@ for hookinput in sys.stdin:
 
         # Pass all this information to "buildbot sendchange"
         if files_arg:
-            p = subprocess.Popen('buildbot sendchange %s --logfile - --master %s --repository %s --revision %s --who "%s" --vc git %s' % (category_arg, master, repo, revhash, author, files_arg), stdin=subprocess.PIPE, shell=True)
+            p = subprocess.Popen('/srv/buildbot/master_env/bin/buildbot sendchange %s --branch master --logfile - --master %s --repository %s --revision %s --who "%s" --vc git %s' % (category_arg, master, repo, revhash, author, files_arg), stdin=subprocess.PIPE, shell=True)
             p.communicate(input=log.encode())
